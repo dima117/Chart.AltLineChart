25a1b62581797516a9eee418b0bbfb815874d859
Janne Hamalainen <janne.hamalainen@leadin.fi>
2015-08-18 19:32:03 +0300

    Datasets can have own width for strokes or disable it by setting it to zero

--- Chart.Scatter.js
+++ Chart.Scatter.js
@@ -74,6 +74,7 @@
 						ctx.strokeStyle = this.strokeColor;
 						ctx.lineWidth = this.strokeWidth;
 
+
 						ctx.fillStyle = this.fillColor;
 
 						ctx.fill();
@@ -207,7 +208,7 @@
 
 		// LINES
 		datasetStroke: true,				// Boolean - Whether to show a stroke for datasets
-		datasetStrokeWidth: 2,				// Number - Pixel width of dataset stroke
+		datasetStrokeWidth: 0,				// Number - Pixel width of dataset stroke
 		datasetStrokeColor: '#007ACC',		// String - Color of dataset stroke
 		datasetPointStrokeColor: 'white',	// String - Color of dataset stroke
 
@@ -633,6 +634,7 @@
 			this.strokeColor = datasetOptions.strokeColor || chartOptions.datasetStrokeColor;
 			this.pointColor = datasetOptions.pointColor || datasetOptions.strokeColor || chartOptions.datasetStrokeColor;
 			this.pointStrokeColor = datasetOptions.pointStrokeColor || chartOptions.datasetPointStrokeColor;
+			this.strokeWidth = datasetOptions.strokeWidth || chartOptions.datasetStrokeWidth;
 
 			this.pointDot = chartOptions.pointDot;
 			this.pointDotRadius = chartOptions.pointDotRadius;
@@ -981,40 +983,48 @@
 			var ctx = this.chart.ctx,
 				prev = undefined;
 
-			ctx.lineJoin = "round";
-			ctx.lineWidth = this.options.datasetStrokeWidth;
-			ctx.strokeStyle = dataset.strokeColor || this.options.datasetStrokeColor;
 
-			ctx.beginPath();
+			if (dataset.strokeWidth==0) {
+				return;
+			}
+			else {
+				ctx.lineJoin = "round";
+				ctx.lineWidth = dataset.strokeWidth || this.options.datasetStrokeWidth;
+				ctx.strokeStyle = dataset.strokeColor || this.options.datasetStrokeColor;
 
-			helpers.each(dataset.points, function (point, index) {
+				ctx.beginPath();
 
-				if (index === 0) {
+				helpers.each(dataset.points, function (point, index) {
 
-					ctx.moveTo(point.x, point.y);
-				}
-				else {
+					if (index === 0) {
 
-					if (this.options.bezierCurve) {
-
-						ctx.bezierCurveTo(
-							prev.controlPoints.x2,
-							prev.controlPoints.y2,
-							point.controlPoints.x1,
-							point.controlPoints.y1,
-							point.x, point.y);
+						ctx.moveTo(point.x, point.y);
 					}
 					else {
 
-						ctx.lineTo(point.x, point.y);
+						if (this.options.bezierCurve) {
+
+							ctx.bezierCurveTo(
+								prev.controlPoints.x2,
+								prev.controlPoints.y2,
+								point.controlPoints.x1,
+								point.controlPoints.y1,
+								point.x, point.y);
+						}
+						else {
+
+							ctx.lineTo(point.x, point.y);
+						}
 					}
-				}
 
-				prev = point;
+					prev = point;
 
-			}, this);
+				}, this);
 
-			ctx.stroke();
+				ctx.stroke();
+
+			}
+
 
 			// debug
 			//if (this.options.bezierCurve) {
--- README.md
+++ README.md
@@ -4,6 +4,26 @@
 
 [Documentation & live demo](http://dima117.github.io/Chart.Scatter/)
 
+
+## Changes in this fork
+
+This fork has option to set line (stroke) width as zero to disable them per dataset.
+E.g.
+
+```javascript
+var datasets = [
+	{
+		label: "dataset 1",
+		strokeWidth: 5,
+		data: data
+	},
+	{
+		label: "dataset 2",
+		strokeWidth: 0, // here we have zero width and only points are drawn
+		data: data2 }
+];
+```
+
 ## License
 
 Chart.Scatter.js is available under the [MIT license](http://opensource.org/licenses/MIT).
